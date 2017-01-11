d6ad5cc1b05cc3730210f538f8fe6a1d00350690
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-11 04:14:09 -0500

    render html

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 function onRequest(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World');
-  res.end();
+  fs.readFile('./index.html', null, function(err, data) {
+    if(err) {
+      res.writeHead(404).write('File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 }
 
 http.createServer(onRequest).listen(8080);  // created server
