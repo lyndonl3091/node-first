3fee470874a8f3840548f477fbf3f17683bd937c
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-11 04:15:08 -0500

    html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 function onRequest(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   fs.readFile('./index.html', null, function(err, data) {
     if(err) {
       res.writeHead(404).write('File not found!');
